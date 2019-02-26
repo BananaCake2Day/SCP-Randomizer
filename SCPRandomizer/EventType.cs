ae5d25eb476b8963b366f80d1fff0ed01225c794
BananaCake2Day <cprootintootinsecrets@gmail.com>
2019-02-25 23:01:36 -0500

    made the plugin

---
+++ SCPRandomizer/EventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[Flags]
+enum EventType : byte
+{
+    None = 0,
+    InventoryEvent = 1,
+    SpawnEvent = 2,
+    RoleChangeEvent = 4,
+    PositionEvent = 8,
+    ServerEvent = 16,
+    SCPEvent = 32
+}
---
+++ SCPRandomizer/RoundHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+class RoundHandler : IEventHandlerRoundEnd, IEventHandlerRoundStart
+{
+    MainPlugin plugin;
+
+    public RoundHandler(MainPlugin pl)
+    {
+        plugin = pl;
+    }
+
+    public void OnRoundEnd(RoundEndEvent ev)
+    {
+        plugin.roundStatus = false;
+    }
+
+    public void OnRoundStart(RoundStartEvent ev)
+    {
+        plugin.roundStatus = true;
+        plugin.RunNextEvent();
+    }
+}
